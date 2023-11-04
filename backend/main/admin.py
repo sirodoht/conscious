40401554b900c7c9fed4b48339cb814b2b813e1f
Theodore Keloglou <zf@sirodoht.com>
2023-11-04 15:53:34 +0000

    add models on admin panel

--- backend/main/admin.py
+++ backend/main/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
 
-# Register your models here.
+from main import models
+
+
+@admin.register(models.User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "username",
+        "email",
+        "date_joined",
+        "last_login",
+    )
+    list_display_links = ("id", "username")
+    search_fields = ("username", "email")
+
+
+@admin.register(models.Participant)
+class ParticipantAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+    )
+
+
+@admin.register(models.Conversation)
+class ConversationAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "title",
+        "active_statement",
+    )
+    list_display_links = ("id", "title")
+    search_fields = ("title", "active_statement")
+
+
+@admin.register(models.Statement)
+class StatementAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "text",
+        "conversation",
+    )
