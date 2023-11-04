bbf790bdcff5d788769422f52434ddab74205a56
Theodore Keloglou <zf@sirodoht.com>
2023-11-04 16:46:06 +0000

    add allowed hosts domain

--- backend/conscious/settings.py
+++ backend/conscious/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = "django-insecure-x$p+)1j0m2uwle4b@9ou#))kfn(8mo17iiu#v6#%ndb&x@*f60
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ.get("DOMAIN_NAME", "")]
 
 
 # Application definition
