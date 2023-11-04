81c851625504b1e377da5d67c0249df4bbbb29ce
Theodore Keloglou <zf@sirodoht.com>
2023-11-04 13:12:56 +0000

    add models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Participant:
+    pass
+
+
+class Conversation(models.Model):
+    title = models.CharField(max_length=200)
+    active_statement = models.ForeignKey(Statement, on_delete=SET_NULL)
+
+
+class Statement(models.Model):
+    text = models.CharField(max_length=300)
+    created_at = models.DateTimeField(auto_add_now=True)
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE)
+
+
+class Vote(models.Model):
+    statement = models.ForeignKey(Statement, on_delete=models.CASCADE)
+    value = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_add_now=True)
+    participant = models.ForeignKey(Participant, on_delete=models.CASCADE)
