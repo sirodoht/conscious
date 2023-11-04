40656130a32d93c6450755383f488373facfd143
Theodore Keloglou <zf@sirodoht.com>
2023-11-04 15:53:41 +0000

    add vote endpoints

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -5,9 +5,10 @@ from main import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("vote/<int:statement_id>/<slug:value>/", views.vote, name="vote"),
 ]
 
 # mobile app API
 urlpatterns += [
-#    path("api/conversations/<conversation_id>/votes/active/"),
+    path("api/conversations/<slug:conversation_id>/votes/active/", views.api_vote, name="api_vote"),
 ]
--- backend/main/views.py
+++ backend/main/views.py
@@ -1,6 +1,69 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from random import randint
+from django.contrib import messages
+import json
+from django import forms
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponseNotAllowed, HttpResponse, HttpResponseBadRequest, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+from main import models
 
 
 def index(request):
-    return HttpResponse("hey")
+    conversation_list = models.Conversation.objects.all()
+    return render(request, "main/index.html", locals())
+
+
+def vote(request, statement_id, value):
+    if request.method != "POST":
+        return HttpResponseNotAllowed(['POST'])
+
+    if value not in ("agree", "disagree", "pass"):
+        return HttpResponseBadRequest()
+    statement = get_object_or_404(models.Statement, id=statement_id)
+    participant = models.Participant.objects.create(public_id="a" + str(randint(100, 999)))
+    vote = models.Vote.objects.create(
+        value=value,
+        statement=statement,
+        participant=participant,
+    )
+    messages.add_message(request, messages.INFO, "vote registered")
+    return redirect("index")
+
+
+@csrf_exempt
+def api_vote(request, conversation_id):
+    if request.method != "POST":
+        return HttpResponseNotAllowed(['POST'])
+
+    data = json.loads(request.body.decode("utf-8"))
+    class VoteForm(forms.Form):
+        user_id = forms.CharField()
+        vote = forms.CharField()
+    form = VoteForm(data)
+    if form.is_valid():
+        participant_public_id = form.cleaned_data["user_id"]
+        vote_value = form.cleaned_data["vote"]
+        if vote_value not in ("agree", "disagree", "pass"):
+            return JsonResponse({
+                "error": {"vote": ["This field can only be 'agree', 'disagree', 'pass'."]},
+            })
+
+        conversation = get_object_or_404(models.Conversation, id=conversation_id)
+        statement = models.Statement.objects.filter(conversation=conversation).latest("created_at")
+        participant, _ = models.Participant.objects.get_or_create(public_id=participant_public_id)
+        vote = models.Vote.objects.create(
+            value=vote_value,
+            statement=statement,
+            participant=participant,
+        )
+        return JsonResponse({
+            "user_id": participant.public_id,
+            "vote": vote.value,
+            "registered_at": vote.created_at,
+            "error": None,
+        })
+    else:
+        return JsonResponse({
+            "error": form.errors,
+        })
